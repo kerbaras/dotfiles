d61ffd82b498a19b23f08d4e786f6cbec778d76c
Matias Pierobon <matias.pierobonm@gmail.com>
2016-12-01 03:31:43 -0300

    fix checkout

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo pacman -S bspwm sxhkd compton mpd mpc mpv ncmpcpp feh --noconfirm
-packer -S lemonbar-xft-git rxvt-unicode-256xresources urxvt-clipboard urxvt-resize-font urxvt-vtwheel --noconfirm
+packer -S lemonbar-xft-git rxvt-unicode-256xresources urxvt-clipboard urxvt-resize-font-git urxvt-vtwheel --noconfirm
 
 ln -s ~/.dotfiles/config/bspwm ~/.config/
 ln -s ~/.dotfiles/config/sxhkd ~/.config/
@@ -10,3 +10,4 @@ ln -s ~/.dotfiles/config/nvim ~/.config/
 cp .dotfiles/local/bin/* .local/bin
 cp .dotfiles/local/share/fonts/* .local/share/fonts
 sudo fc-cache
+sudo pacman -S acpi
